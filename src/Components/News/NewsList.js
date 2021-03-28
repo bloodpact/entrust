82cdf8da060127eeaddaf89053898e501945fcd0
hell hell <toxep@mail.ru>
2021-03-28 13:37:45 +0300

    img sizes

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import NewsItem from './NewsItem'
-import newsArray from '../../Misc/news.json'
+import NewsItem from './NewsItem';
+import newsArray from '../../Misc/news.json';
 
 function NewsList(props) {
-    const [news, setNews] = React.useState([])
-    React.useEffect(()=>{
-        setNews(newsArray)
-    },[])
-    return (
-        <div className='container'>
-            <h1>Новости</h1>
-            {newsArray.map(el=><NewsItem key={el.title} news={el}/>)}
-        </div>
-    );
+	const [news, setNews] = React.useState([]);
+	React.useEffect(() => {
+		setNews(newsArray);
+	}, []);
+	return (
+		<div className="container">
+			<h1>Информационный блок</h1>
+			{newsArray.map((el) => (
+				<NewsItem key={el.title} news={el} />
+			))}
+		</div>
+	);
 }
 
 export default NewsList;
--- src/Components/Products/ProductsList.js
+++ src/Components/Products/ProductsList.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import ProductItem from './ProductItem';
-import hm10_b from '../../Misc/products/hm10_b.jpg';
-import temperature from '../../Misc/products/датчик температуры.jpg';
-import pressure from '../../Misc/products/датчики давления.jpg';
-import defend from '../../Misc/products/защитные гильзы.jpg';
-import manometr from '../../Misc/products/манометры.jpg';
-import divider from  '../../Misc/products/разделители сред.jpg';
-import consumption from '../../Misc/products/расходомеры.jpg';
-import level from '../../Misc/products/уровнемеры.jpg';
-
+import hm10_b from '../../Misc/products/hm10.png';
+import temperature from '../../Misc/products/temp.png';
+import pressure from '../../Misc/products/pressure.png';
+import defend from '../../Misc/products/defend.png';
+import manometr from '../../Misc/products/manometr.png';
+import divider from '../../Misc/products/divide.png';
+import consumption from '../../Misc/products/comsuption.png';
+import level from '../../Misc/products/level.png';
 
 function ProductsList(props) {
-    const [products, setProudcts] = React.useState([])
-    React.useEffect(()=>{
-        setProudcts([
-            {imgSrc:hm10_b, imgTitle:'hm10_b'},
-            {imgSrc:temperature, imgTitle:'датчик температуры'},
-            {imgSrc:pressure, imgTitle:'датчики давления'},
-            {imgSrc:defend, imgTitle:'защитные гильзы'},
-            {imgSrc:manometr, imgTitle:'манометры'},
-            {imgSrc:divider, imgTitle:'разделители сред'},
-            {imgSrc:consumption, imgTitle:'расходомеры'},
-            {imgSrc:level, imgTitle:'уровнемеры'},
-        ])
-    },[])
-    return (
-        <div className="container">
-            <h1>Продукция</h1>
-            <div className='col s12 m7'>
-                {products.map(el=><ProductItem key={el.imgTitle} products={el}/>)}
-                
-            </div>
-        </div>
-    );
+	const [products, setProudcts] = React.useState([]);
+	React.useEffect(() => {
+		setProudcts([
+			{ imgSrc: hm10_b, imgTitle: 'hm10_b' },
+			{ imgSrc: temperature, imgTitle: 'датчик температуры' },
+			{ imgSrc: pressure, imgTitle: 'датчики давления' },
+			{ imgSrc: defend, imgTitle: 'защитные гильзы' },
+			{ imgSrc: manometr, imgTitle: 'манометры' },
+			{ imgSrc: divider, imgTitle: 'разделители сред' },
+			{ imgSrc: consumption, imgTitle: 'расходомеры' },
+			{ imgSrc: level, imgTitle: 'уровнемеры' },
+		]);
+	}, []);
+	return (
+		<div className="container">
+			<h1>Продукция</h1>
+			<div className="flex">
+				{products.map((el) => (
+					<ProductItem key={el.imgTitle} products={el} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default ProductsList;
