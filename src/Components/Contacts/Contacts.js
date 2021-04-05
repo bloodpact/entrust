fa931004efbaa5888cab0bc9c09a8e4f5ef2f2dd
hell hell <toxep@mail.ru>
2021-04-06 00:27:22 +0300

    edit prods and news

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
 function Contacts(props) {
-    return (
-        <div className='container'>
-            <div className="row">
-                <h1>Контакты</h1>
-                <p>Телефон: 8-888-888-88-88</p>
-                <p>e-mail: email@email</p>
-            </div>
-            <div className="row">
-                <p>Адрес: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, alias.</p>
-                <p>Схема проезда:</p>
-                <div  className='map'>
-                    <a href="https://yandex.ru/maps/213/moscow/?utm_medium=mapframe&utm_source=maps">Москва</a>
-                <a href="https://yandex.ru/maps/213/moscow/house/skakovaya_ulitsa_36/Z04YcwBjQUAGQFtvfXt2d3lkZw==/?ll=37.573852%2C55.776573&utm_medium=mapframe&utm_source=maps&z=16">Скаковая улица, 36 — Яндекс.Карты</a>
-                <iframe src="https://yandex.ru/map-widget/v1/-/CCUUUOHrPA" title='map' width="560" height="400" ></iframe>
-                
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="container">
+			<div className="row">
+				<h1>Контакты</h1>
+				<p>Телефон: 8-888-888-88-88</p>
+				<p>e-mail: email@email</p>
+			</div>
+			<div className="row">
+				<p>Адрес: Санкт-Петербург, В.О., Средний проспект 4</p>
+				<p>Схема проезда:</p>
+
+				<div className="map">
+					<a href="https://yandex.ru/maps/2/saint-petersburg/?utm_medium=mapframe&utm_source=maps">
+						Санкт‑Петербург
+					</a>
+					<a href="https://yandex.ru/maps/2/saint-petersburg/?feedback=map%2Fedit&ll=30.289332%2C59.945320&utm_medium=mapframe&utm_source=maps&z=16.05">
+						Яндекс.Карты
+					</a>
+					<iframe
+						title="map"
+						src="https://yandex.ru/map-widget/v1/-/CCUUrAFT-B"
+						width="560"
+						height="400"></iframe>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Contacts;
--- src/Components/Products/ProductItem.js
+++ src/Components/Products/ProductItem.js
@@ -1,25 +1,31 @@
 import React from 'react';
 
 function ProductItem(props) {
-    return (
-        <div className="card sm12">
-            <div className="card-image waves-effect waves-block waves-light">
-             <img className="activator small-img" alt={props.products.imgTitle} src={props.products.imgSrc}/>
-            </div>
-            <div className="card-content">
-                <span className="card-title activator grey-text text-darken-4">{props.products.imgTitle}<i className="material-icons right">more_vert</i></span>
-            </div>
-            <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">
-                    {props.products.imgTitle}<i className="material-icons right">close</i>
-                </span>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Quae perferendis fugit dolores corrupti illum, nobis dolorum. Aspernatur,
-                accusantium molestiae?
-                Eveniet pariatur culpa nulla ullam ipsam laborum dolores facilis aperiam enim?</p>
-            </div>
-        </div>
-    );
+	console.log(props.products.imgSrc);
+	return (
+		<div className="card sm12">
+			<div className="card-image waves-effect waves-block waves-light">
+				<img
+					className="activator small-img"
+					alt={props.products.imgTitle}
+					src={props.products.imgSrc}
+				/>
+			</div>
+			<div className="card-content">
+				<span className="card-title activator grey-text text-darken-4">
+					{props.products.imgTitle}
+					<i className="material-icons right">more_vert</i>
+				</span>
+			</div>
+			<div className="card-reveal">
+				<span className="card-title grey-text text-darken-4">
+					{props.products.imgTitle}
+					<i className="material-icons right">close</i>
+				</span>
+				<p>{props.products.desc}</p>
+			</div>
+		</div>
+	);
 }
 
 export default ProductItem;
